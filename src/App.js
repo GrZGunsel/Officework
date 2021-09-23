692356add1ca0c665a345655cb612e1aaca309c8
kshitz moktan <you@example.com>
2021-09-23 15:45:03 +0545

    sad 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import logo from "./Component/Image/logo.svg";
 import "./App.scss";
 import Login from "./Component/Login/Login";
+import { useState } from "react";
+import { BiSun } from "react-icons/bi";
+import { BsMoon } from "react-icons/bs";
 
 function App() {
+  const [darkmode, setdarkmode] = useState("lightmode");
+
+  const Darkmode = () => {
+    setdarkmode("darkmode");
+  };
+  const Lightmode = () => {
+    setdarkmode("lightmode");
+  };
+
+  console.log({ darkmode });
   return (
-    <div className="mainbody">
-      <Login Logo={logo} />
+    <div className={` mainbody ${darkmode}`}>
+      <div className="authentication">
+        {darkmode === "lightmode" ? (
+          <BsMoon
+            size="43"
+            onClick={Darkmode}
+            className="button"
+            style={{ color: "black" }}
+          />
+        ) : (
+          <BiSun size="43" onClick={Lightmode} className="button" />
+        )}
+        <Login Logo={logo} />
+      </div>
     </div>
   );
 }
